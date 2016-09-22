222b98522c109625053cebce583d92eef836ed5c
Pete Haddrell <petejhaddrell@gmail.com>
2016-09-22 17:25:53 +0100

    Xcode Project
    Created the Xcode project.

---
+++ ann_music/ann_music/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  ann_music
+//
+//  Created by Pete Haddrell on 22/09/2016.
+//  Copyright © 2016 Pete Haddrell. All rights reserved.
+//
+
+#include <iostream>
+
+int main (int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
